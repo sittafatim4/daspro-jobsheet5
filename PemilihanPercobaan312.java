6b5bc99e9fd86716dfd5dd0546b060f710b24eb1
sittafatim4 <144087010+sittafatim4@users.noreply.github.com>
2023-09-26 13:42:10 +0700

    Add files via upload

---
+++ PemilihanPercobaan312.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class PemilihanPercobaan312 {
+   public static void main(String[] args) {
+        double angka1, angka2, hasil;
+        char operator;
+        Scanner input12 = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input12.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input12.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = input12.next().charAt(0);
+        switch (operator) {
+            case '+':
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + "=" + hasil);
+            break;
+            case '-':
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+            break;
+            case '/':
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+            break;
+        }
+   }
+}
